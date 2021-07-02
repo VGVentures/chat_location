87b42f46b2ec4863e94843c454e6d9f895f0f34b
Felix Angelov <felangelov@gmail.com>
2021-07-01 23:35:56 -0500

    app bar title

--- lib/channel_list/view/channel_list_page.dart
+++ lib/channel_list/view/channel_list_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:chat_location/l10n/l10n.dart';
 
 class ChannelListPage extends StatelessWidget {
   const ChannelListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.channelListAppBarTitle)),
+    );
   }
 }
