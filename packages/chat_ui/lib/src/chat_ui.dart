a024f8e65287677469dd508ae2049087726c6f99
Felix Angelov <felangelov@gmail.com>
2021-07-01 23:41:11 -0500

    chore(chat_ui): create package:chat_ui

---
+++ packages/chat_ui/lib/src/chat_ui.dart
@@ -0,0 +1,7 @@
+/// {@template chat_ui}
+/// Flutter package which exposes reusable chat UI components
+/// {@endtemplate}
+class ChatUi {
+  /// {@macro chat_ui}
+  const ChatUi();
+}
---
+++ packages/chat_ui/test/chat_ui_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter_test/flutter_test.dart';
+import 'package:chat_ui/chat_ui.dart';
+
+void main() {
+  group('ChatUi', () {
+    test('can be instantiated', () {
+      expect(ChatUi(), isNotNull);
+    });
+  });
+}
