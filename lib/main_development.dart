85c144e29b420c9290a33050dd53af780e23852f
Felix Angelov <felangelov@gmail.com>
2021-07-01 23:11:38 -0500

    refactor: startup

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,31 +5,15 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'dart:async';
-import 'dart:developer';
-
-import 'package:bloc/bloc.dart';
+import 'package:chat_location/bootstrap.dart';
 import 'package:chat_repository/chat_repository.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:chat_location/app/app.dart';
-import 'package:chat_location/app/app_bloc_observer.dart';
 
-void main() {
-  Bloc.observer = AppBlocObserver();
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
-
-  final chatClient = StreamChatClient(
-    const String.fromEnvironment('STREAM_API_KEY'),
-    logLevel: Level.OFF,
-  );
-
-  final chatRepository = ChatRepository(chatClient: chatClient);
-
-  runZonedGuarded(
-    () => runApp(App(chatRepository: chatRepository)),
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+void main() async {
+  await bootstrap(
+    builder: ({required ChatRepository chatRepository}) async {
+      return App(chatRepository: chatRepository);
+    },
   );
 }
