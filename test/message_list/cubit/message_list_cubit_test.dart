b591e449dbb8f31c39b643e41c11f905028b4242
Felix Angelov <felangelov@gmail.com>
2021-07-09 12:47:19 -0500

    bloc and state tests

--- test/message_list/cubit/message_list_cubit_test.dart
+++ test/message_list/cubit/message_list_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:chat_location/message_list/message_list.dart';
 import 'package:chat_repository/chat_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -29,6 +30,72 @@ void main() {
           equals(MessageListState(channel: channel)),
         );
       });
+    });
+
+    group('locationRequested', () {
+      const latitude = 42.0;
+      const longitude = 13.37;
+      const coordinatePair = CoordinatePair(
+        latitude: latitude,
+        longitude: longitude,
+      );
+      blocTest<MessageListCubit, MessageListState>(
+        'emits [pending, unavailable] when location is unavailable',
+        build: () {
+          when(
+            () => chatRepository.getCurrentLocation(),
+          ).thenThrow(Exception());
+          return MessageListCubit(
+            channel: channel,
+            chatRepository: chatRepository,
+          );
+        },
+        act: (cubit) => cubit.locationRequested(),
+        expect: () => [
+          MessageListState(
+            channel: channel,
+            location: const CurrentLocation(
+              status: CurrentLocationStatus.pending,
+            ),
+          ),
+          MessageListState(
+            channel: channel,
+            location: const CurrentLocation(
+              status: CurrentLocationStatus.unavailable,
+            ),
+          )
+        ],
+      );
+
+      blocTest<MessageListCubit, MessageListState>(
+        'emits [pending, available] when location is available',
+        build: () {
+          when(
+            () => chatRepository.getCurrentLocation(),
+          ).thenAnswer((_) async => coordinatePair);
+          return MessageListCubit(
+            channel: channel,
+            chatRepository: chatRepository,
+          );
+        },
+        act: (cubit) => cubit.locationRequested(),
+        expect: () => [
+          MessageListState(
+            channel: channel,
+            location: const CurrentLocation(
+              status: CurrentLocationStatus.pending,
+            ),
+          ),
+          MessageListState(
+            channel: channel,
+            location: const CurrentLocation(
+              latitude: latitude,
+              longtitude: longitude,
+              status: CurrentLocationStatus.available,
+            ),
+          )
+        ],
+      );
     });
   });
 }
